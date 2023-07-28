4e78f4e0dbe4906dfb3c5787e4b3248c03081259
VittoriaDespo <105251001+VittoriaDespo@users.noreply.github.com>
2023-07-28 17:33:19 +0000

    adding chart function

--- budget.py
+++ budget.py
@@ -1,6 +1,7 @@
 class Category:
   ledger = []
   category = ""
+  withdraws = 0
 
   def __init__(self, category):
     self.category = category 
@@ -23,6 +24,7 @@ class Category:
   def withdraw (self,amount,description=""):
     if self.check_funds(amount):
       self.ledger.append({"amount": -amount, "description": description})
+      self.withdraws += amount
       return True 
     else: return False
  
@@ -36,6 +38,7 @@ class Category:
     if self.check_funds(amount):
       self.withdraw(amount,"Transfer to " + name.category)
       name.deposit(amount,"Transfer from " + self.category)
+      self.withdraws += amount
       return True
     else:
       return False
@@ -47,9 +50,42 @@ class Category:
     else:
       return False
       
-  
-    
-  
 
 def create_spend_chart(categories):
-  return None
+    chart = 'Percentage spent by category\n'
+    category = Category
+    expenses = dict()
+    total = 0
+    percentage = []
+    count = 0
+    max_len = 0
+    for category in categories:
+        expenses[category.category] = category.withdraws
+        total += category.withdraws
+        count += 1
+
+    for k, v in expenses.items():
+        percentage.append(v / total * 100)
+        if len(k) > max_len:
+            max_len = len(k)
+
+    for i in range(100, -1, -10):
+        chart += f'{str(i).rjust(3)}|'
+        for percent in percentage: 
+            if percent > i:
+                chart += ' o '  
+            else:
+                chart += ' ' * 3
+        chart += " \n"
+                    
+    chart += '    ' + ('-' * (count * 3 + 1)) + '\n'
+    for i in range(max_len):
+        chart += ' ' * 4
+        for k in expenses.keys():
+            if i < len(k):
+                chart += ' ' + k[i] + ' '
+            else:
+              chart += " " * 3  
+        chart += " \n"     
+    return chart.rstrip("\n") 
+                    
